2182da06dbe9aea13995ae853760c698c2bd7d5e
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2021-08-24 23:16:18 +0100

    Added initial styling & page structure

--- tip-calculator-app/tailwind.config.js
+++ tip-calculator-app/tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
 	theme: {
 		colors: {
 			background: "hsl(185, 41%, 84%)",
+			primary: "hsl(172, 67%, 45%)",
+			text: "hsl(186, 14%, 43%)",
+			lightText: "hsl(184, 14%, 56%)",
+			white: "hsl(0, 0%, 100%)",
+			dark: "hsl(183, 100%, 15%)",
+			inputBG: "hsl(189, 41%, 97%)",
+			disabled: "rgba(13,104,109,255)",
+			disabledText: "rgba(3,93,98,255)",
 		},
 		fontFamily: {
 			body: ['"Space Mono"'],
