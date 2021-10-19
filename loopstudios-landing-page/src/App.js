a94c9ae34e25d611b87f5869fb4b1eda32888b9e
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2021-10-19 18:09:30 +0100

    Made the second section responsive

--- loopstudios-landing-page/src/App.js
+++ loopstudios-landing-page/src/App.js
@@ -7,11 +7,17 @@ function App() {
 			<div className="px-12 xl:px-56 pt-12 xl:pt-24 h-screen md:h-auto bg-mobileHero md:bg-hero bg-no-repeat bg-center bg-cover bg-blend-multiply md:bg-gray-400">
 				<Header />
 			</div>
-			<div className="px-56 h-screen flex items-center relative">
-				<div className="relative w-full h-2/3">
-					<img className="h-full left-0" src={imageInteractive} alt="" />
-					<div className="bg-white pt-28 pl-28 absolute left-1/2 bottom-0">
-						<h2 className="inline text-6xl font-josefin uppercase">
+			<div className="px-12 xl:px-56 h-screen flex items-center">
+				<div className="relative w-full lg:h-2/3 flex flex-col lg:flex-row">
+					<div className="flex-1">
+						<img
+							className="object-cover w-full h-auto lg:max-h-full lg:w-auto lg:h-full"
+							src={imageInteractive}
+							alt=""
+						/>
+					</div>
+					<div className="bg-white pt-14 lg:pl-14 xl:pt-28 xl:pl-28 2xl:absolute left-1/2 bottom-0 flex-1 text-center lg:text-left">
+						<h2 className="inline text-5xl lg:text-6xl font-josefin uppercase">
 							The Leader In <br />
 							Interactive VR
 						</h2>
